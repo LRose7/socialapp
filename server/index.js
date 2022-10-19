0d43d55e085ee2d4a593fa3ba14e5c53a103b7f6
LRose7 <lindseyurquhart@tutanota.com>
2022-10-19 12:01:46 -0400

    set up bare bones backend server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
+if (process.env.NODE_ENV = 'production') {
+    require('dotenv').config();
+}
+const express = require('express');
 
+// set up express app
+const app = express(); 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+});
