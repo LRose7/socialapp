6f70e6d0275f53da72f3814a668c92ccd7ef61e6
LRose7 <lindseyurquhart@tutanota.com>
2022-10-18 20:29:27 -0400

    finished Profile page

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -4,6 +4,8 @@ import Cover from "../../img/cover.jpg";
 import Profile from "../../img/profileImg.jpg";
 
 const ProfileCard = () => {
+  const ProfilePage = () => true;
+
   return (
     <div className="ProfileCard">
       <div className="ProfileImages">
@@ -24,15 +26,24 @@ const ProfileCard = () => {
             <span>Followings</span>
           </div>
           <div className="vl"></div>
-            <div className="follow">
-              <span>1</span>
-              <span>Followers</span>
-            </div>
+          <div className="follow">
+            <span>1</span>
+            <span>Followers</span>
+          </div>
+
+          {ProfilePage && (
+            <>
+              <div className="vl"></div>
+              <div className="follow">
+                <span>3</span>
+                <span>Posts</span>
+              </div>
+            </>
+          )}
         </div>
         <hr />
       </div>
-
-      <span>My Profile</span>
+      {ProfilePage ? "" : <span>My Profile</span>}
     </div>
   );
 };
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PostSide from '../../components/Postside/PostSide';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
 import ProfileLeft from '../../components/ProfileLeft/ProfileLeft';
+import RightSide from '../../components/RightSide/RightSide';
 import './Profile.css';
 
 const Profile = () => {
   return (
     <div className="Profile">
         <ProfileLeft />
+
+        <div className="Profile-center">
+            <ProfileCard />
+            <PostSide />
+        </div>
+
+        <RightSide />
     </div>
   )
 }
